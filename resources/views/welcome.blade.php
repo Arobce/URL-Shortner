5bb9ae99c62bdb5d59f6e82b24aabf7faea2748c
Roshan Chapagain <arobce@gmail.com>
2019-05-29 19:50:01 +0545

    Added Fields

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,90 +10,44 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css" integrity="sha256-ujE/ZUB6CMZmyJSgQjXGCF4sRRneOimQplBVLu8OU5w=" crossorigin="anonymous" />
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            .shorten-url-button{
+                width: 90%;
+                border-color: white;
+                color: white;
             }
 
-            .full-height {
-                height: 100vh;
+            .input-container{
+                background-color: #2A5BD7;
+        
             }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
+            .space-left{
+                margin-left: 10px;
             }
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <h1 class="title has-text-centered">URL Shortner</h1>
+        <div class="app-wrapper">
+            
+            <div class="input-container">
+                <div class="columns is-mobile">
+                    <div class="column is-four-fifths">
+                        <input type="text" name="url-to-shorten" class="space-left input is-medium" placeholder="Enter URL to Shorten">
+                    </div>
+                    <div class="column is-one-fifth">
+                        <a class="button is-outlined is-white shorten-url-button space-right is-medium">Shorten</a>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+               
+                <div class="shortened-url-div">
+                        <input type="text" name="shortened-url"  class="input  is-medium" placeholder="Shortened URL">
                 </div>
             </div>
+            
         </div>
     </body>
 </html>
